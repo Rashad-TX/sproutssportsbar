8a2330da854adef67538181d80c520bf14e8b12c
Rashad Moore <rjm4god7@gmail.com>
2022-05-04 08:09:00 -0500

    updated menu

--- js.js
+++ js.js
@@ -4,3 +4,25 @@ const navbarLinks = document.getElementsByClassName('nav-items')[0]
 toggleButton.addEventListener('click', ()=> {
     navbarLinks.classList.toggle('active')
 })
+
+const accordHeaders = document.querySelectorAll(".accord-header");
+accordHeaders.forEach((ach) => {
+    ach.addEventListener("click", toggleItem,false);
+});
+
+function toggleItem(){
+    const currentContentElement=this.nextElementSibling;
+
+    const isCollapsed = currentContentElement.classList.contains("collapse");
+
+    accordHeaders.forEach ((ach) => {
+        const contentElement = ach.nextElementSibling;
+        if(!contentElement.classList.contains("collapse")) {
+        contentElement.classList.add("collapse");
+        }
+
+        });
+if(isCollapsed){
+    currentContentElement.classList.remove("collapse");
+}
+}
